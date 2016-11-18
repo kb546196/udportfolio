795bec53698a5e1340c9d831621738daa61bae84
kb546196 <KB@kates-mbp.home>
2016-11-18 15:40:18 +0000

    More changes to Sliding pictures
    Minimised pizza.png and moved height and width out of loop and into to
    css

--- views/js/main.js
+++ views/js/main.js
@@ -506,9 +506,9 @@ document.addEventListener('DOMContentLoaded', function() {
   for (var i = 0; i < rows*cols; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizza.png";
-    elem.style.height = "100px";
-    elem.style.width = "73.333px";
+    //made smaller version of png for background
+    elem.src = "images/pizza_small.png";
+    //moved the height and width out of the loop and added it to  css 
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
